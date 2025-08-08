92e7f47a4dd6f7247446f43f315105d6f3492a2f
GitHub Action [rubocop] <action@github.com>
2025-08-08 16:16:18 +0000

    style: rubocop自動修正 [skip ci]

--- backend/test/mailers/user_mailer_test.rb
+++ backend/test/mailers/user_mailer_test.rb
@@ -8,9 +8,9 @@ class UserMailerTest < ActionMailer::TestCase
       password: "password123"
     )
     verification_url = "http://localhost:3001/verify-email?token=test_token"
-    
+
     mail = UserMailer.email_verification(user, verification_url)
-    
+
     assert_equal "【おうちの畑】メールアドレス認証のお願い", mail.subject
     assert_equal [ "test@example.com" ], mail.to
     assert_equal [ "ouchi.no.hatake@gmail.com" ], mail.from
