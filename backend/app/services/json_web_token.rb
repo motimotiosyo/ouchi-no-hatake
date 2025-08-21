9e6465c8c1e31cba4aa992a679f017ae24c4fc7e
田畑友作 <kuppa0713@icloud.com>
2025-08-22 02:39:35 +0900

    feat: JWT有効期限を24時間から6ヶ月に変更

--- backend/app/services/json_web_token.rb
+++ backend/app/services/json_web_token.rb
@@ -2,7 +2,7 @@ class JsonWebToken
   SECRET_KEY = Rails.application.credentials.secret_key_base.to_s
 
   # トークン発行
-  def self.encode(payload, exp = 24.hours.from_now)
+  def self.encode(payload, exp = 6.months.from_now)
     payload[:exp] = exp.to_i
     payload[:jti] = SecureRandom.uuid
     JWT.encode(payload, SECRET_KEY)
