6f924f4a6367170855d9e567d01c4cdaa7578762
GitHub Action [rubocop] <action@github.com>
2025-10-19 00:16:25 +0000

    style: rubocop自動修正 [skip ci]

--- backend/db/migrate/20251018020323_add_phase_to_guide_steps.rb
+++ backend/db/migrate/20251018020323_add_phase_to_guide_steps.rb
@@ -3,6 +3,6 @@ class AddPhaseToGuideSteps < ActiveRecord::Migration[7.2]
     add_column :guide_steps, :phase, :integer, null: false, default: 0
     add_column :guide_steps, :applicable_to, :string, null: false, default: 'all'
 
-    add_index :guide_steps, [:guide_id, :phase]
+    add_index :guide_steps, [ :guide_id, :phase ]
   end
 end
