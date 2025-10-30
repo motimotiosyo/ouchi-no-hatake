0e8920045a032eeb5fb014094d92a934021600ee
田畑友作 <kuppa0713@icloud.com>
2025-10-31 06:02:12 +0900

    fix: Render.comのホストをホワイトリストに追加

--- backend/config/environments/production.rb
+++ backend/config/environments/production.rb
@@ -115,4 +115,5 @@ Rails.application.configure do
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
   config.hosts << ENV["FRONTEND_URL"] if ENV["FRONTEND_URL"].present?
+  config.hosts << "ouchi-no-hatake.onrender.com"
 end
